cc3a5669c7ba0ffd35d028d976b610c1d9d22c8a
TaisukeFujise <tafujise3502@icloud.com>
2026-03-02 00:45:47 +0900

    fix: Add philo initializer and test thread process

--- philo/init/init.c
+++ philo/init/init.c
@@ -6,37 +6,78 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 20:57:42 by tafujise          #+#    #+#             */
-/*   Updated: 2026/03/01 23:51:34 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/03/02 00:35:06 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+int		init_fork_mutex(t_ctx *ctx);
+int		init_philo(t_ctx *ctx);
 void	free_fork_mutex(int num_of_philo, pthread_mutex_t *fork_mutex);
 
 int	init_ctx(t_ctx *ctx)
 {
-	int	i;
-
 	ctx->is_dead = false;
 	if (pthread_mutex_init(&ctx->dead_mutex, NULL) != 0)
 		return (perror("dead_mutex init"), FAILURE);
 	if (pthread_mutex_init(&ctx->print_mutex, NULL) != 0)
 		return (perror("print_mutex init"),
 			pthread_mutex_destroy(&ctx->dead_mutex), FAILURE);
+	if (init_fork_mutex(ctx) == FAILURE)
+		return (pthread_mutex_destroy(&ctx->dead_mutex),
+			pthread_mutex_destroy(&ctx->print_mutex), FAILURE);
+	if (init_philo(ctx) == FAILURE)
+		return (pthread_mutex_destroy(&ctx->dead_mutex),
+			pthread_mutex_destroy(&ctx->print_mutex),
+			free_fork_mutex(ctx->config.num_of_philo, ctx->fork_mutex),
+			FAILURE);
+	return (SUCCESS);
+}
+
+int	init_fork_mutex(t_ctx *ctx)
+{
+	int	i;
+
 	ctx->fork_mutex = malloc(sizeof(pthread_mutex_t)
 			* ctx->config.num_of_philo);
 	if (ctx->fork_mutex == NULL)
-		return (perror("malloc"), pthread_mutex_destroy(&ctx->dead_mutex),
-			pthread_mutex_destroy(&ctx->print_mutex), FAILURE);
+		return (FAILURE);
 	i = 0;
 	while (i < ctx->config.num_of_philo)
 	{
 		if (pthread_mutex_init(&ctx->fork_mutex[i], NULL) != 0)
-			return (perror("malloc"), pthread_mutex_destroy(&ctx->dead_mutex),
-				pthread_mutex_destroy(&ctx->print_mutex),
-				free_fork_mutex(ctx->config.num_of_philo, ctx->fork_mutex),
-				FAILURE);
+			return (free_fork_mutex(i, ctx->fork_mutex), FAILURE);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	init_philo(t_ctx *ctx)
+{
+	int	i;
+	int	j;
+
+	ctx->philo = malloc(sizeof(t_philo) * ctx->config.num_of_philo);
+	if (ctx->philo == NULL)
+		return (FAILURE);
+	i = 0;
+	while (i < ctx->config.num_of_philo)
+	{
+		ctx->philo[i].philo_id = i;
+		ctx->philo[i].eat_count = 0;
+		ctx->philo[i].last_meal_time = 0;
+		ctx->philo[i].ctx = ctx;
+		if (pthread_mutex_init(&ctx->philo[i].last_meal_time_mutex, NULL) != 0)
+		{
+			j = 0;
+			while (j < i)
+			{
+				pthread_mutex_destroy(&ctx->philo[j].last_meal_time_mutex);
+				j++;
+			}
+			return (FAILURE);
+		}
 		i++;
 	}
 	return (SUCCESS);
--- philo/main.c
+++ philo/main.c
@@ -6,26 +6,50 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 04:46:21 by tafujise          #+#    #+#             */
-/*   Updated: 2026/03/01 23:53:47 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/03/02 00:38:24 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// void	*philo_action(void *arg)
-// {
-// }
+void	*philo_action(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	printf("id = %d\n", philo->philo_id);
+	return (NULL);
+}
 
 int	main(int argc, char **argv)
 {
 	t_ctx	ctx;
+	int		i;
+	int		err;
 
-	// int		err;
 	if (parse_args(argc, argv, &ctx.config) == FAILURE)
 		return (1);
 	if (init_ctx(&ctx) == FAILURE)
 		return (1);
-	// err = pthread_create(&tid, NULL, philo_action, NULL);
+	i = 0;
+	while (i < ctx.config.num_of_philo)
+	{
+		err = pthread_create(&ctx.philo[i].tid, NULL, philo_action,
+				&ctx.philo[i]);
+		if (err != 0)
+			return (1);
+		i++;
+	}
+	i = 0;
+	while (i < ctx.config.num_of_philo)
+	{
+		err = pthread_join(ctx.philo[i].tid, NULL);
+		printf("tid = %lu\n", ctx.philo[i].tid);
+		if (err != 0)
+			return (1);
+		i++;
+	}
+	sleep(1);
 	puts("happy");
 	return (0);
 }
